20ec25ba03ce8b92647c0f15f1f9d95ece63b41e
user <user@DESKTOP-3DNUHQ9>
2019-11-14 13:49:08 +0900

    초기 커밋.

---
+++ 소스.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int i;
+	int j;
+
+	for (i = 1; i <= 5; i++) {
+		for (j = 5; i <= j; j--) {
+			printf("%d", j);
+	 }
+		printf("\n");
+	}
+
+
+
+
+
+
+
+}
